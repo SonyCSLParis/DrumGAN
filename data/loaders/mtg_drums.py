9269a45cad881713f1726231f433d8425c2965e9
jnistal <j.nistalhurle@gmail.com>
2020-04-08 13:13:10 +0200

    Reimplement some loader methods for mtg db

--- data/loaders/mtg_drums.py
+++ data/loaders/mtg_drums.py
@@ -9,6 +9,33 @@ from ..db_extractors.mtg_drums import extract
 
 
 class MTGDrums(AudioDataLoader):
+    def __getitem__(self, index):
+        labels = torch.Tensor(self.metadata[index]).float()
+        if self.getitem_processing:
+            return self.getitem_processing(self.data[index]), labels
+        else:
+            return self.data[index], labels
+
     def load_data(self):
         self.data, self.metadata, self.header = \
             extract(self.data_path, self.criteria)
+    
+    def index_to_labels(self, batch, transpose=False):
+        if transpose:
+            return list(zip(*batch))
+        return batch
+
+    def get_validation_set(self, batch_size=None, process=False):
+        if batch_size is None:
+            batch_size = len(self.val_data)
+        val_batch = self.val_data[:batch_size]
+
+        val_label_batch = self.val_labels[:batch_size]
+        if process:
+            val_batch = \
+                torch.stack([self.getitem_processing(v) for v in val_batch])
+        return val_batch, torch.Tensor(val_label_batch).float()
+
+    def get_random_labels(self, batch_size):
+        return torch.rand((batch_size, len(self.header['attributes'])))
+
