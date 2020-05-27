4cc8d51df29750b2d6e0e9d76a27d881afcc642c
javier <j.nistalhurle@gmail.com>
2020-05-27 18:18:58 +0200

    Update generate.py

--- generate.py
+++ generate.py
@@ -8,23 +8,23 @@ if __name__ == "__main__":
                         description='Generation testing script', 
                         add_help=False)
     parser.add_argument('generation_test', type=str,
-                        help='Name of the generation test to launch. To get \
+                        help='Name of the generation test (check evaluation/gen_tests) to launch. To get \
                         the arguments specific to an generation test please \
                         use: eval.py evaluation_name -h')
     parser.add_argument('-d', '--dir', help="Path to model's root folder",
                         type=str, dest="dir")
-    parser.add_argument('-n', '--nsynth-path', 
-                        help="Path to nsynth dataset root folder",
-                        type=str, dest="nsynth_path")
     parser.add_argument('-o', '--out-dir', help='Output directory',
-                        type=str, dest="outdir", default="output_networks")
+                        type=str, dest="outdir", default="")
     parser.add_argument('-m', '--midi', help='Path to midi file',
                         type=str, dest="midi", 
                         default="./test_midi_files/midi_furelisa_adapted.mid")
-    parser.add_argument('--n_gen', help='Path to midi file',
+    parser.add_argument('-N', '--n-gen', help='Path to midi file',
                         type=int, dest="n_gen", 
                         default=10)
-
+    parser.add_argument('--batch-size', help='Path to midi file',
+                        type=int, dest="batch_size", 
+                        default=50)
+    
     if len(sys.argv) > 1 and sys.argv[1] in ['-h', '--help']:
         parser.print_help()
         sys.exit()
